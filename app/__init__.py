b7292a3a642c59319f60456ca588f918c5e64be1
Avery Rozar <avery.rozar@insecure-it.com>
2017-07-17 14:11:52 -0400

    change configuration file to be an importable module

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from os import getenv
-from lib.config_parser import ConfigParser
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker
@@ -7,28 +6,34 @@ from socket import gethostbyaddr, herror
 import splunklib.client as client
 import syslog
 
-__version__ = '0.4'
+# ---------------------
+# get the configuration
+# ---------------------
+import app.config.configuration as config
+
+db_config = {'drivername': config.db_drivername,
+             'host': config.db_host,
+             'database': config.database,
+             'username': config.db_username,
+             'password': config.db_password}
+
+
+# --------
+# app info
+# --------
+__version__ = '0.5'
 __author__ = 'Avery Rozar: avery.rozar@insecure-it.com'
 
-config_file = getenv('PERCEPTION_CONFIG')
-home_dir = getenv('HOME')
-
-if config_file is None:
-    config_file = '/etc/perception/configuration.yml'
-
-config = ConfigParser(config_file).parse()
-db_config = {'drivername': config['drivername'],
-             'host': config['host'],
-             'database': config['database'],
-             'username': config['username'],
-             'password': config['password']}
-
+# -----------------------
 # Connect to the database
+# -----------------------
 engine = create_engine(URL(**db_config), pool_size=20)
 Session = sessionmaker(bind=engine)
 db_session = Session()
 
+# ----------------
 # pexpect SSH info
+# ----------------
 SSH_NEW_KEY = '.Are you sure you want to continue connecting (yes/no)?'
 SSH_BAD_KEY = 'WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!'
 SSH_REFUSED = 'Connection refused'
@@ -37,15 +42,21 @@ SSH_OUTDATED_PROTOCOL = 'Protocol major versions differ: 2 vs. 1\\r\\r\\n'
 PASSWORD = '[P|p]assword'
 PERMISSION_DENIED = 'Permission denied, please try again.'
 
+# -----------
 # ssh prompts
+# -----------
 GT_PROMPT = '>$'
 HASH_PROMPT = '#$'
 
+# ----------------------
 # Cisco generic commands
+# ----------------------
 SHOWVER = 'show version'
 SHOW_OS = 'show version | include Software'
 
+# ------------------
 # Cisco IOS commands
+# ------------------
 IOS_TERMLEN0 = 'terminal length 0'
 IOS_SHOW_CDP_DETAIL = 'show cdp neighbors detail'
 IOS_SHOW_ADJACENCY = 'show adjacency'
@@ -60,16 +71,30 @@ IOS_RTR_SHOW_SERIALNUM = 'show version | include Processor board ID'
 IOS_SHOW_LICS = 'show version | include License Level'
 IOS_LAST_RESORT_SHOW_MODEL = 'show version  | include (WS)'
 
+# ------------------
 # Cisco ASA commands
+# ------------------
 ASA_TERMPAGER0 = 'terminal pager 0'
 ASA_SHOWARP = 'show arp'
 ASA_SHOW_LOCAL_CONNECTIONS = 'show route | in C'
 ASA_SHOW_XLATE = 'show xlate'
 ASA_SHOW_CONN = 'show conn'
 
+# ----------------
+# environment vars
+# ----------------
+home_dir = getenv('HOME')
 tmp_dir = '/tmp/perception/'
 rsinfrastructure_tmp_dir = '%srsinfrastructure/' % tmp_dir
 nmap_tmp_dir = '%snmap/' % tmp_dir
+
+# ----------
+# ipv4 regex
+# ----------
+ipv4 = r'\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
+       r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
+       r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
+       r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
 
 
 def hostname_lookup(ip_addr):
@@ -81,15 +106,15 @@ def hostname_lookup(ip_addr):
     return hostname
 
 
-# TODO: Use rabbit MQ, I'm loosing events
-def splunk_sock(event):
+# TODO: Use rabbit MQ, I'm losing events
+def splunk_sock(event, index_name):
     try:
-        s = client.connect(host=config['splunk_host'],
-                           port=config['splunk_api_port'],
-                           username=config['splunk_username'],
-                           password=config['splunk_password'])
+        s = client.connect(host=config.splunk_host,
+                           port=config.splunk_api_port,
+                           username=config.splunk_username,
+                           password=config.splunk_password)
 
-        index = s.indexes[config['splunk_index']]
+        index = s.indexes[index_name]
 
         with index.attached_socket(sourcetype='perception_app') as sock:
             sock.send(str(event))
