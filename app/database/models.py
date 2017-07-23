cc0385709303e45d58117126a51cd0efa76a7411
Avery Rozar <avery.rozar@insecure-it.com>
2017-07-23 08:06:08 -0400

    add perception_product_uuid to all tables. remove from discovery_protocol_findings interface, port_id, discovery_version, protocol_hello, vtp_domian, native_vlan, duplex and power_draw. remove from rsinfrastucture os_version, license_level, system_serial_number, model_number, and last_investigation.

--- app/database/models.py
+++ app/database/models.py
@@ -15,7 +15,8 @@ class OpenvasAdmin(Base):
     __tablename__ = 'openvas_admin'
 
     id = Column(Integer, primary_key=True, nullable=False)
-    username = Column(Text, unique=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
+    username = Column(Text, nullable=False)
     password = Column(postgresql.UUID, nullable=False)
     created_at = Column(TIMESTAMP(timezone=False), default=_get_date)
     updated_at = Column(TIMESTAMP(timezone=False), onupdate=_get_date)
@@ -25,6 +26,7 @@ class SvcUser(Base):
         __tablename__ = 'svc_users'
 
         id = Column(Integer, primary_key=True, nullable=False)
+        perception_product_uuid = Column(postgresql.UUID, nullable=False)
         username = Column(String, nullable=False, unique=True)
         description = Column(String)
 
@@ -32,9 +34,12 @@ class SvcUser(Base):
         updated_at = Column(TIMESTAMP(timezone=False), onupdate=_get_date)
 
         def __init__(self,
+                     perception_product_uuid,
                      username=None,
                      description=None,
                      openvas_lsc_id=None):
+
+            self.perception_product_uuid = perception_product_uuid
 
             if description:
                 self.description = description
@@ -50,6 +55,7 @@ class RSInfrastructure(Base):
     __tablename__ = 'rsinfrastructure'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     ip_addr = Column(postgresql.INET, unique=True, nullable=False)
     host_name = Column(Text, unique=True)
 
@@ -57,31 +63,18 @@ class RSInfrastructure(Base):
     svc_user_id = Column(Integer, ForeignKey('svc_users.id'))
     svc_users = relationship('SvcUser', backref='rsinfrastructure', order_by=id)
 
-    os_version = Column(Text)
-    license_level = Column(Text)
-    system_serial_number = Column(Text, unique=True)
-    model_number = Column(Text)
-    last_investigation = Column(TIMESTAMP(timezone=False))
-
     created_at = Column(TIMESTAMP(timezone=False), default=_get_date)
     updated_at = Column(TIMESTAMP(timezone=False), default=_get_date)
 
-    def __init__(self, svc_user_id,
+    def __init__(self,
+                 perception_product_uuid,
+                 svc_user_id,
                  ip_addr,
-                 host_name=None,
-                 os_version=None,
-                 license_level=None,
-                 system_serial_number=None,
-                 model_number=None,
-                 last_investigation=None):
+                 host_name=None):
 
+        self.perception_product_uuid = perception_product_uuid
         self.svc_user_id = svc_user_id
         self.ip_addr = ip_addr
-        self.os_version = os_version
-        self.license_level = license_level
-        self.system_serial_number = system_serial_number
-        self.model_number = model_number
-        self.last_investigation = last_investigation
 
         if host_name:
             self.host_name = host_name
@@ -91,6 +84,7 @@ class RSAddr(Base):
     __tablename__ = 'rsaddrs'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
 
     """Relation to rsinfrastructure"""
     rsinfrastructure_id = Column(Integer, ForeignKey('rsinfrastructure.id'))
@@ -106,23 +100,15 @@ class DiscoveryProtocolFinding(Base):
     __tablename__ = 'discovery_protocol_findings'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
 
     """Relation to rsinfrastructure"""
     rsinfrastructure_id = Column(Integer, ForeignKey('rsinfrastructure.id'), nullable=False)
     rsinfrastructure = relationship('RSInfrastructure', backref='discovery_protocol_findings', order_by=id)
 
-    remote_device_id = Column(Text, nullable=False)
     ip_addr = Column(postgresql.INET)
     platform = Column(Text)
     capabilities = Column(Text)
-    interface = Column(Text)
-    port_id = Column(Text)
-    discovery_version = Column(Integer)
-    protocol_hello = Column(Text)
-    vtp_domain = Column(Text)
-    native_vlan = Column(Integer)
-    duplex = Column(Text)
-    power_draw = Column(Text)
     created_at = Column(TIMESTAMP(timezone=False), default=_get_date)
 
 
@@ -130,6 +116,7 @@ class SeedRouter(Base):
     __tablename__ = 'seed_routers'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     ip_addr = Column(postgresql.INET, unique=True, nullable=False)
     host_name = Column(Text, unique=True)
 
@@ -139,10 +126,13 @@ class SeedRouter(Base):
 
     created_at = Column(TIMESTAMP(timezone=False), default=_get_date)
 
-    def __init__(self, svc_user_id,
+    def __init__(self,
+                 perception_product_uuid,
+                 svc_user_id,
                  ip_addr,
                  host_name=None):
 
+        self.perception_product_uuid = perception_product_uuid
         self.svc_user_id = svc_user_id
         self.ip_addr = ip_addr
 
@@ -154,6 +144,7 @@ class OpenvasLastUpdate(Base):
     __tablename__ = 'openvas_last_updates'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     updated_at = Column(TIMESTAMP(timezone=False), nullable=False)
 
 
@@ -161,6 +152,7 @@ class DoNotSeed(Base):
     __tablename__ = 'do_not_seeds'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     ip_addr = Column(postgresql.INET, unique=True, nullable=False)
 
 
@@ -168,6 +160,7 @@ class HostUsingSshv1(Base):
     __tablename__ = 'hosts_using_sshv1'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     ip_addr = Column(postgresql.INET, unique=True, nullable=False)
 
 
@@ -175,6 +168,7 @@ class HostWithBadSshKey(Base):
     __tablename__ = 'hosts_with_bad_ssh_key'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     ip_addr = Column(postgresql.INET, unique=True, nullable=False)
 
 
@@ -182,6 +176,7 @@ class NmapHost(Base):
     __tablename__ = 'nmap_hosts'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     ip_addr = Column(postgresql.INET, unique=True, nullable=False)
     created_at = Column(TIMESTAMP(timezone=False), default=_get_date)
 
@@ -190,5 +185,6 @@ class OpenVasVuln(Base):
     __tablename__ = 'openvas_vulns'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    perception_product_uuid = Column(postgresql.UUID, nullable=False)
     name = Column(Text, unique=True, nullable=False)
     created_at = Column(TIMESTAMP(timezone=False), default=_get_date)
