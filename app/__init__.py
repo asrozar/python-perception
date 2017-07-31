3eebc8d596d83d23e0b3b6752c33e208b97907c1
Avery Rozar <avery.rozar@insecure-it.com>
2017-07-31 12:08:23 -0400

    add product_uuid add es_add_document add check_if_valid_cider add get_or_create

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from os import getenv
+from uuid import UUID
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker
 from socket import gethostbyaddr, herror
+from re import match
 import splunklib.client as client
 import syslog
+import json
+import httplib
+
 
 # ---------------------
 # get the configuration
@@ -16,6 +21,15 @@ db_config = {'drivername': config.db_drivername,
              'database': config.database,
              'username': config.db_username,
              'password': config.db_password}
+
+with open('/etc/product_uuid', 'r') as f:
+    try:
+        system_uuid = f.read().rstrip()
+        UUID(system_uuid)
+    except ValueError:
+        syslog.syslog(syslog.LOG_INFO, 'Error: System UUID not found')
+        print('Error: System UUID not found')
+        exit(99)
 
 
 # --------
@@ -88,25 +102,61 @@ tmp_dir = '/tmp/perception/'
 rsinfrastructure_tmp_dir = '%srsinfrastructure/' % tmp_dir
 nmap_tmp_dir = '%snmap/' % tmp_dir
 
-# ----------
-# ipv4 regex
-# ----------
-ipv4 = r'\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
-       r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
-       r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
-       r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
-
 
 def hostname_lookup(ip_addr):
     try:
         hostname = gethostbyaddr(ip_addr)[0]
     except herror:
-        hostname = 'Unknown hostname'
+        hostname = None
 
     return hostname
 
 
-# TODO: Use rabbit MQ, I'm losing events
+def es_add_document(es_host, es_port, doc_index, doc_type, doc_id, doc):
+
+    try:
+        headers = {'Accept': 'text/plain',
+                   'Content-type': 'application/json'}
+
+        conn = httplib.HTTPConnection(es_host, es_port)
+
+        if doc_id is None:
+            conn.request('POST', '/%s/%s?' % (doc_index, doc_type), headers=headers, body=doc)
+
+        elif doc_id is not None:
+            conn.request('PUT', '/%s/%s/%s?' % (doc_index, doc_type, doc_id), headers=headers, body=doc)
+
+        resp = conn.getresponse()
+        data = resp.read()
+
+        json_resp = json.loads(data)
+
+        if resp.status == 400:
+            syslog.syslog(syslog.LOG_INFO, str(json_resp))
+
+        elif resp.status == 403:
+            syslog.syslog(syslog.LOG_INFO, str(json_resp))
+
+        elif resp.status == 404:
+            syslog.syslog(syslog.LOG_INFO, str(json_resp))
+
+        elif resp.status == 409:
+            syslog.syslog(syslog.LOG_INFO, str(json_resp))
+
+        elif resp.status == 412:
+            syslog.syslog(syslog.LOG_INFO, str(json_resp))
+
+        elif resp.status == 500:
+            syslog.syslog(syslog.LOG_INFO, str(json_resp))
+
+        elif resp.status == 503:
+            syslog.syslog(syslog.LOG_INFO, str(json_resp))
+
+    except Exception as es_add_data_e:
+        syslog.syslog(syslog.LOG_INFO, 'es_add_document error: %s' % str(es_add_data_e))
+        syslog.syslog(syslog.LOG_INFO, 'es_add_document event: %s' % str(str(doc)))
+
+
 def splunk_sock(event, index_name):
     try:
         s = client.connect(host=config.splunk_host,
@@ -118,6 +168,70 @@ def splunk_sock(event, index_name):
 
         with index.attached_socket(sourcetype='perception_app') as sock:
             sock.send(str(event))
-    except Exception as e:
-        syslog.syslog(syslog.LOG_INFO, 'splunk_sock error: %s' % str(e))
+        return 0
+
+    except Exception as splunk_sock_e:
+        syslog.syslog(syslog.LOG_INFO, 'splunk_sock error: %s' % str(splunk_sock_e))
         syslog.syslog(syslog.LOG_INFO, 'splunk_sock event: %s' % str(event))
+        return 99
+
+
+def check_if_valid_cider(cider):
+
+    # is a valid ipv4 cider?
+    if match(r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.)'
+             r'{3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$', cider):
+        return True
+
+    # is a valid ipv6 cider?
+    if match(r'^s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|'
+             r'((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:)'
+             r'{5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]'
+             r'?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?#'
+             r'((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:)'
+             r'{3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)'
+             r'(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|'
+             r'((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|'
+             r':))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|'
+             r'2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|'
+             r'((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|'
+             r':)))(%.+)?s*(\/(d|dd|1[0-1]d|12[0-8]))$', cider):
+        return True
+
+    return False
+
+
+def check_if_valid_address(ipaddr):
+
+    # is a valid ipv4 address?
+    if match(r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.)'
+             r'{3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$', ipaddr):
+        return True
+
+    # is a valid ipv6 address?
+    elif match(r'^s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:)'
+               r'{6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:)'
+               r')|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]d|1dd|[1-9]?d)'
+               r'(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4})'
+               r'{1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]'
+               r'?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}'
+               r':((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:)'
+               r'{2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)'
+               r'(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|'
+               r'((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|'
+               r':))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)'
+               r'(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:)))(%.+)?s*', ipaddr):
+        return True
+
+    return False
+
+
+def get_or_create(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.commit()
+        return instance
