1208cbf173f663c73eee82c2ff109374de5a857f
Avery Rozar <avery.rozar@insecure-it.com>
2017-08-02 13:05:17 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="Perception",
+
+    # Version number (initial):
+    version="0.5",
+
+    # Application author details:
+    author="Avery Rozar",
+    author_email="avery.rozar@critical-sec.com",
+
+    # Packages
+    packages=["app"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://www.critical-sec/com/perception",
+
+    #
+    license="LICENSE.txt",
+    description="",
+
+    long_description=open("README.md").read(),
+
+    # Dependent packages (distributions)
+    install_requires=['alembic',
+                      'sqlalchemy',
+                      'scapy',
+                      'pyOpenSSL',
+                      'psycopg2',
+                      'cryptography',
+                      'pexpect',
+                      'pika',
+                      'xlsxwriter',
+                      'splunk-sdk',
+                      'elasticsearch',
+                      ],
+)
