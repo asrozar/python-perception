ac2f4f1bba2feb12f79818ff5d66c776313b52fb
Avery Rozar <avery.rozar@insecure-it.com>
2017-08-02 14:00:57 -0400

    add install_requirements

--- setup.py
+++ setup.py
@@ -25,4 +25,18 @@ setup(
     description="",
 
     long_description=open("README.md").read(),
+
+    # Dependent packages (distributions)
+    install_requires=['alembic',
+                      'sqlalchemy',
+                      'scapy',
+                      'pyOpenSSL',
+                      'psycopg2',
+                      'cryptography',
+                      'pexpect',
+                      'pika',
+                      'xlsxwriter',
+                      'splunk-sdk',
+                      'elasticsearch',
+                      ],
 )
