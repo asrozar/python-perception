5902defef5a94ccc99e4f3a34aed2d6213d25924
Avery Rozar <avery.rozar@insecure-it.com>
2017-08-06 06:03:37 -0400

    add from perception.shared.variables import db_config and remove offline migrations

--- perception/database/migrations/env.py
+++ perception/database/migrations/env.py
@@ -3,7 +3,7 @@ from alembic import context
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from logging.config import fileConfig
-from perception import db_config, config as myconfig
+from perception.shared.variables import db_config
 
 
 # this is the Alembic Config object, which provides
@@ -25,33 +25,6 @@ target_metadata = None
 # my_important_option = config.get_main_option("my_important_option")
 # ... config.
 
-# build URL
-sqlalchemy_url = '%s://%s:%s@%s/%s' % (myconfig.db_drivername,
-                                       myconfig.db_username,
-                                       myconfig.db_password,
-                                       myconfig.db_host,
-                                       myconfig.database)
-
-
-def run_migrations_offline():
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option(sqlalchemy_url)
-    context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True)
-
-    with context.begin_transaction():
-        context.run_migrations()
-
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
@@ -71,7 +44,4 @@ def run_migrations_online():
         with context.begin_transaction():
             context.run_migrations()
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+run_migrations_online()
