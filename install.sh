d9a7c828bb38e5fc7473d6ba12f38c68f2c63de9
Avery Rozar <avery.rozar@insecure-it.com>
2017-08-06 06:04:22 -0400

    add contained install

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ if [ $EUID -ne 0 ]; then
 fi
 
 kernal=$(uname -r)
+hostname=$(hostname)
+install_log="/tmp/python-perception-install.log"
+es_pgp_key="deb https://artifacts.elastic.co/packages/5.x/apt stable main"
+es_config="/etc/elasticsearch/elasticsearch.yml"
 unsupported="Perception is built for Kali Linux, this system is unsupported"
 dmi_product_id="/sys/devices/virtual/dmi/id/product_uuid"
 product_uuid="/etc/product_uuid"
@@ -20,7 +24,6 @@ generator_msg="# generated via install.sh"
 importsys="import sys"
 ifnamemain="if __name__ == \"__main__\":"
 py_sysexit="    sys.exit(main())"
-adduser_conf="/etc/adduser.conf"
 etc_perception="/etc/perception/"
 perception_config="/usr/local/lib/python2.7/dist-packages/perception/config/"
 perceptiond="/usr/bin/perceptiond"
@@ -35,8 +38,11 @@ then
     exit 1
 fi
 
+read -r -p "[!] Is this installation of Perception for a contained install? [Y/N]: " contained_input
+
 # build the sdist package
-python setup.py sdist > /tmp/python-perception-sdist.log 2> /dev/null
+python setup.py sdist > ${install_log}
+chmod 666 ${install_log}
 
 if [ $? -eq 0 ];
 
@@ -44,15 +50,15 @@ then
     perception_zip=$(ls -1 dist | tr '\n' '\0' | xargs -0 -n 1 basename)
 
     # make sure pip2 is installed
-    which pip2 > /dev/null
+    which pip2 >> ${install_log}
 
     if [ $? -ne 0 ];
     then
-        apt-get install python-pip -y > /tmp/python-pip-install.log 2> /dev/null;
+        apt-get install python-pip -y >> ${install_log}
     fi
 
     # use pip2 to install or upgrade
-    pip2 install --upgrade dist/${perception_zip} > /tmp/python-perception-install.log 2> /dev/null;
+    pip2 install --upgrade dist/${perception_zip} >> ${install_log}
 
     if [ $? -eq 0 ];
     then
@@ -61,8 +67,8 @@ then
         if [ ! -L ${etc_perception} ];
 
             then
-                mkdir ${etc_perception}
-                ln -s ${perception_config} ${etc_perception} ;
+                mkdir ${etc_perception} >> ${install_log}
+                ln -s ${perception_config} ${etc_perception} >> ${install_log}
 
         fi
 
@@ -104,35 +110,40 @@ fi
 
 if [[ ! -f "/etc/systemd/system/perceptiond.service" ]];
         then
-            cp ${perceptiond_service} "/etc/systemd/system/perceptiond.service"
+            cp ${perceptiond_service} "/etc/systemd/system/perceptiond.service" >> ${install_log}
             if [[ ! -f ${etc_perception}"configuration.py" ]]
                 then
-                    cp ${perception_config}"configuration-example.py" ${etc_perception}"config/configuration.py"
-                    chmod 640 ${etc_perception}"config/configuration.py"
+                    cp ${perception_config}"configuration-example.py" ${etc_perception}"config/configuration.py" >> ${install_log}
+                    chmod 640 ${etc_perception}"config/configuration.py" >> ${install_log}
             fi
 fi
 
-read -r -p "Is this installation of Perception for a contained install? [Y/N]: " contained_input
-
 case ${contained_input} in
     [nN][oO][nN])
-        echo
-        # wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add
-        # apt-get install apt-transport-https
-        # echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
-        # apt-get update && apt-get install -y elasticsearch rabbitmq-server openjdk-8-jdk
-        # edit elasticsearch config
-        # enable elasticsearch and rabbitmq
-        # run migration
+    echo -e ${end_msg}
+    exit 0
+
 esac
 
-read -r -p "Would you like to use Perception CLI as the default shell when adduser is invoked? [Y/N]: " cli_input
+DBPASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+psql_func(){
+    su postgres
+    psql -c "CREATE DATABASE perceptiondb;" >> ${install_log}
+    psql -c "CREATE USER perceptiondb_user WITH PASSWORD '${DBPASSWD}';" >> ${install_log}
+    psql -c "ALTER DATABASE perceptiondb OWNER TO perceptiondb_user;" >> ${install_log}
+    sed -i "s/perceptiondb_user_password/${DBPASSWD}/" ${etc_perception}"config/configuration.py" >> ${install_log}
+    exit
+}
 
-case ${cli_input} in
-    [nN][oO][nN])
-        echo -e ${end_msg}
-        exit 1;
-esac
-
-sed -i "s/DSHELL=\/bin\/bash/DSHELL=\/usr\/bin\/perception_cli/" ${adduser_conf}
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add - >> ${install_log}
+apt-get install apt-transport-https -y >> ${install_log}
+echo ${es_pgp_key} >> -a /etc/apt/sources.list.d/elastic-5.x.list
+apt-get update && apt-get install -y elasticsearch rabbitmq-server openjdk-8-jdk >> ${install_log}
+sed -i "s/#cluster.name: my-application/cluster.name: perception_cluster/" ${es_config} >> ${install_log}
+sed -i "s/#node.name: node-1/node.name: ${hostname}/" ${es_config} >> ${install_log}
+systemctl enable postgresql-server.service elasticsearch.service rabbitmq-server.service >> ${install_log}
+systemctl start postgresql-server.service elasticsearch.service rabbitmq-server.service >> ${install_log}
+psql_func
+python run_migrations.py >> ${install_log}
 echo -e ${end_msg}
+exit 0
