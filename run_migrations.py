e66ca5352df6c42b03b699ca2f7fc855def6a2fe
Avery Rozar <avery.rozar@insecure-it.com>
2017-08-06 13:30:53 -0400

    add sqlalchemy connection and statements to create db user

--- run_migrations.py
+++ run_migrations.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python2
 from alembic.config import Config
 from alembic import command
-alembic_cfg = Config("/usr/local/lib/python2.7/dist-packages/perception/database/alembic.ini")
-command.upgrade(alembic_cfg, "head")
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+import sys
+import subprocess
+
+
+def main():
+
+    perceptiondb_user_password = sys.argv[1]
+
+    subprocess.call(['createdb', '-U', 'postgres', 'perceptiondb'])
+    engine = create_engine('postgresql://postgres@localhost/perceptiondb')
+
+    conn = engine.connect()
+    create_perceptiondb_user = text("CREATE USER perceptiondb_user WITH password '%s';" % perceptiondb_user_password)
+    alter_perceptiondb = text("ALTER DATABASE perceptiondb OWNER TO perceptiondb_user;")
+
+    conn.execute(create_perceptiondb_user)
+    conn.execute(alter_perceptiondb)
+    conn.close()
+
+    alembic_cfg = Config("/usr/local/lib/python2.7/dist-packages/perception/database/alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+
+if __name__ == "__main__":
+    main()
